d0c85fe9b4333b7007c81a9195932e59178c9f1c
DavidAOchoa <od174140@gmail.com>
2022-01-28 18:42:43 -0600

    updated footer

--- PortfolioV2/src/app/core/footer/footer.component.ts
+++ PortfolioV2/src/app/core/footer/footer.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { endWith } from 'rxjs';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('secondSection', { static: true }) secondSection: ElementRef<HTMLDivElement>;
 
+   constructor(@Inject(DOCUMENT) private document: Document) {}
+  
   ngOnInit(): void {
+    this.initialAnimations();
+    this.initScrollAnimations();    
+  }
+
+  initialAnimations(): void {
+    gsap.to(this.document.querySelector('.footer'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.footer'),
+        scrub: 1.5,
+        toggleClass: 'active',
+        start: 'top center',
+        end: "+= 200"
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+  }
+
+  initScrollAnimations(): void {
+
   }
 
 }
